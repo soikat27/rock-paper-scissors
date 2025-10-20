6e87f8db5a22236036ae2ecbfa87f3ac23c6e93c
Soikat <193371447+soikat27@users.noreply.github.com>
2025-10-19 22:51:56 -0400

    Implement getComputerChoice()

---
+++ src/javascript.js
@@ -0,0 +1,16 @@
+function getComputerChoice ()
+{
+    let choice = Math.floor((Math.random() * 3)) + 1;
+
+    switch (choice)
+    {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+        default:
+            return ""
+    }
+}
